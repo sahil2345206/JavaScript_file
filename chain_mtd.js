801381aeaeb9c61cb1cf1084ae63da7ae444ef22
unknown <sahdhi184@gmail.com>
2023-03-22 14:08:30 +0530

    chain mtd update

--- chain_mtd.js
+++ chain_mtd.js
@@ -1,20 +1,35 @@
-class dog{
-    is=null
-    log(){
-        console.log(this)
-    }
-    bark(){
-        this.is="woffing"
-        this.log();
-        return this
-    }
-    walk(){
-        this.is="walking"
-        this.log()
-        return this
+// class dog{
+//     is=null
+//     log(){
+//         console.log(this)
+//     }
+//     bark(){
+//         this.is="woffing"
+//         this.log();
+//         return this
+//     }
+//     walk(){
+//         this.is="walking"
+//         this.log()
+//         return this
+//     }
+// }
+ 
+// dog2=new dog()
+// console.log(dog2.bark())
+// console.log(dog2.bark().walk( ))
+
+function createPerson(name , age){
+   
+const person = { 
+    name : name,
+    age : age,
+    greeting : function(){
+        console.log(`hello my name is ${this.name} and my age is ${this.age} `)
     }
 }
- 
-dog2=new dog()
-console.log(dog2.bark())
-console.log(dog2.bark().walk( ))
+return person;
+}
+
+const  person1= createPerson("sahil" , 25);
+person1.greeting();
