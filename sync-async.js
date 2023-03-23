b4a0ead19cf5bfd342f81c7f50ed31f9f8f796cc
unknown <sahdhi184@gmail.com>
2023-03-23 12:48:00 +0530

    sync-async pdate

--- sync-async.js
+++ sync-async.js
@@ -1,4 +1,4 @@
-// synchronous example
+// // synchronous example
 
 // const fun2 = () =>{
 //     console.log('function2 is start')
@@ -13,15 +13,57 @@
 
 
 
-const fun2 = () =>{
-    setTimeout(() => {
-        console.log('function2 is start')
-    },3000)
-}
-const fun1 = () =>{
-    console.log('function1 is start')
-    fun2()
-    console.log('function1 is end')
+// // asynchronous example
+
+// const fun2 = () =>{
+//     setTimeout(() => {
+//         console.log('function2 is start')
+//     },3000)
+// }
+// const fun1 = () =>{
+//     console.log('function1 is start')
+//     fun2()
+//     console.log('function1 is end')
+// }
+
+// fun1()
+
+
+
+
+// let name1="sahil"
+
+// console.log(`my name is  ${name1}`)
+
+// function call_name(name){
+//     console.log(`name call in function ${name}`)
+// }
+
+// call_name(name1)
+
+
+
+let name1="sahil"
+
+setTimeout(()=>{
+    console.log(`my name is  ${name1}`)
+},10)
+
+function call_name(name){
+    console.log(`name call in function ${name}`)
 }
 
-fun1()
+call_name(name1)
+
+arr =[1,2,3,4,5]
+for(i=1;i<100;i++){
+    console.log(arr)
+}
+
+arr =[1,2,3,4,5]
+for(i=1;i<100;i++){
+    console.log(arr)
+}
+
+
+
