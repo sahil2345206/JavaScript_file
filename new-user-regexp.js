d061f3e811e366725ce5f1e17e21af0bba08a779
unknown <sahdhi184@gmail.com>
2023-03-28 15:02:45 +0530

    update

--- new-user-regexp.js
+++ new-user-regexp.js
@@ -1,3 +1,25 @@
+// // // // 
+
+
+// // // class user{
+// // //   constructor(email,password){
+// // //     this.email = email;
+// // //     this.password = password;
+// // //   }
+// // //   validateEmail(){
+// // //     const emailRegexp = /(.)(@)/
+// // //     if(!emailRegexp.test(this.email)){
+// // //       console.log(`${this.email} is not valid email adress`)
+// // //     } else{
+// // //       console.log(`${this.email} is valid email adress`)
+// // //     }
+// // //   }
+// // // }
+
+// // // sahil = new user('sahil@gmail.com')
+// // // sahil.validateEmail()
+
+
 // // // 
 
 
@@ -8,7 +30,7 @@
 // //   }
 // //   validateEmail(){
 // //     const emailRegexp = /(.)(@)/
-// //     if(!emailRegexp.test(this.email)){
+// //     if(!this.email.match(emailRegexp)){
 // //       console.log(`${this.email} is not valid email adress`)
 // //     } else{
 // //       console.log(`${this.email} is valid email adress`)
@@ -20,69 +42,71 @@
 // // sahil.validateEmail()
 
 
-// // 
+// const prompt = require('prompt');
 
-
-// class user{
-//   constructor(email,password){
+// class User {
+//   constructor(email, password) {
 //     this.email = email;
 //     this.password = password;
 //   }
-//   validateEmail(){
-//     const emailRegexp = /(.)(@)/
-//     if(!this.email.match(emailRegexp)){
-//       console.log(`${this.email} is not valid email adress`)
-//     } else{
-//       console.log(`${this.email} is valid email adress`)
+
+//   validateEmail() {
+//     const emailRegexp =/^\S+@\S+\.\S+$/
+//     ;
+//     if (!this.email.match(emailRegexp)) {
+//       console.log(`${this.email} is not a valid email address`);
+//     } else {
+//       console.log(`${this.email} is a valid email address`);
+//     }
+//   }
+
+
+//   validatePassword() {
+//     const passwordRegexp =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+//     ;
+//     if (!this.password.match(passwordRegexp)){
+//       console.log(`${this.password} is not a valid password`);
+//     } else {
+//       console.log(`${this.password} is a valid password`);
 //     }
 //   }
 // }
 
-// sahil = new user('sahil@gmail.com')
-// sahil.validateEmail()
+// prompt.start();
+
+// prompt.get(['email', 'password'], function (err, result) {
+//   if (err) { return onErr(err); }
+//   const user = new User(result.email, result.password);
+//   user.validateEmail();
+//   user.validatePassword();
+// });
+
+// function onErr(err) {
+//   console.log(err);
+//   return 1;
+// }
 
 
-const prompt = require('prompt');
 
-class User {
-  constructor(email, password) {
-    this.email = email;
-    this.password = password;
+const token = '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';
+const username = 'sahil2345206';
+const url = `https://api.github.com/users/${username}`;
+
+fetch(url, {
+  headers: {
+    'Authorization': `token ${token}`
   }
-
-  validateEmail() {
-    const emailRegexp =/^\S+@\S+\.\S+$/
-    ;
-    if (!this.email.match(emailRegexp)) {
-      console.log(`${this.email} is not a valid email address`);
-    } else {
-      console.log(`${this.email} is a valid email address`);
-    }
+})
+.then(response => {
+  
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
-
-
-  validatePassword() {
-    const passwordRegexp =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
-    ;
-    if (!this.password.match(passwordRegexp)){
-      console.log(`${this.password} is not a valid password`);
-    } else {
-      console.log(`${this.password} is a valid password`);
-    }
-  }
-}
-
-prompt.start();
-
-prompt.get(['email', 'password'], function (err, result) {
-  if (err) { return onErr(err); }
-  const user = new User(result.email, result.password);
-  user.validateEmail();
-  user.validatePassword();
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
 });
-
-function onErr(err) {
-  console.log(err);
-  return 1;
-}
-
